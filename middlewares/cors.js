59e91c92af3367df34289050596715b3abc57179
danya2903 <167182730+danya2903@users.noreply.github.com>
2024-05-23 10:55:38 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,8 @@ const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'http://localhost:3000',
-    'http://localhost:3001'
+    'http://localhost:3001',
+    'https://danya2903-frontend.nomoredomainswork.ru'
 ];
 
 function cors(req, res, next) {
